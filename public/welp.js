be036b340ac08f72cd0fb55ecac266d4c1f00354
Dafydd Francis <DaveOrdref@gmail.com>
2011-12-16 20:48:13 +0000

    more appropriate js event

--- public/welp.js
+++ public/welp.js
@@ -1,5 +1,5 @@
 $(function () {
-    var input = $('input[type=text]').change(function () {
+    var input = $('input[type=text]').keypress(function () {
         $('img').attr('src', '/images/' + (input[0].value || ' ') + '/' + (input[1].value || ' '));
     });
 });
